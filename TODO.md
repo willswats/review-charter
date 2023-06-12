25b9c23476c280d1d1050cdcfabf8dfd40913e3b
willswats <williamstuwatson@gmail.com>
2023-06-12 16:52:59 +0100

    Add link to AniList user

--- TODO.md
+++ TODO.md
@@ -6,15 +6,14 @@
 
 ## Medium Priority
 
-- [ ] Add link to AniList user
 - [ ] Statistic SVGs
-- [ ] Banner
-- [ ] Change global strings to objects
 - [ ] Hover mouse on charts and categories
+- [ ] Make charts scrollable on mobile size
 
 ## Low Priority
 
+- [ ] Banner
+- [ ] Change global strings to objects
 - [ ] Refresh button
 - [ ] Preserve state between pages or use the url for usernames
-- [ ] Make charts scrollable on mobile size
 - [ ] Make loading icons for each piece of user data
--- src/features/user/components/UserInfo/index.tsx
+++ src/features/user/components/UserInfo/index.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 import styles from "./styles.module.css";
 
 interface UserInfo {
@@ -8,6 +10,19 @@ interface UserInfo {
 export const UserInfo = ({ avatarUrl, userName }: UserInfo) => {
   return (
     <div className={styles["user-info"]}>
+      <a
+        className={styles["user-info__link"]}
+        href={`https://anilist.co/user/${userName}`}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Image
+          src="assets/link-round.svg"
+          width={25}
+          height={25}
+          alt="AniList link"
+        />
+      </a>
       <img className={styles["user-info__avatar"]} src={avatarUrl} />
       <h1>{userName} </h1>
     </div>
