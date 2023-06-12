86793ead3f991ffd634f94869ec489c9fffea15a
willswats <williamstuwatson@gmail.com>
2023-06-12 13:45:29 +0100

    Move head to _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,31 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          name="description"
+          content="Visualise AniList data in multiple charts."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="site.webmanifest" />
+      </Head>
       <body>
         <Main />
         <NextScript />
