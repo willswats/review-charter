dd871b9bc104f60af5930541a32dfaa62a658640
willswats <williamstuwatson@gmail.com>
2023-06-13 13:35:25 +0100

    Adjust search, add svgr

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,6 @@ export default function Document() {
           name="description"
           content="Visualise AniList data in multiple charts."
         />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
