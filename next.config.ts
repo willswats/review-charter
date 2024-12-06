9cda17675bba5b46ba208d5b013c954c6fca171c
willswats <william@williamwatson.dev>
2024-12-06 00:08:46 +0000

    feat: add back next config settings

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "s4.anilist.co",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
